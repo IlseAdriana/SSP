3c5575adba0f17705cee96f78678883940bd7867
IlseAdriana <ilseadrianapa@gmail.com>
2023-05-03 22:37:06 -0600

    get_weigths function was added to problem module

--- Problem.py
+++ Problem.py
@@ -1,10 +1,26 @@
 import numpy as np
 from pymoo.core.problem import Problem
+from pymoo.algorithms.soo.nonconvex.pso import PSO
+from pymoo.optimize import minimize
 
-class myProblem(Problem):
+class ProblemaEsfera(Problem):
     
     def __init__(self, n_var, xl, xu):
         super().__init__(n_var=n_var, xl=xl, xu=xu)
 
     def _evaluate(self, x, out, *args, **kwargs):
         out['F'] = np.sum(x**2, axis=1)
+
+
+# Función que utiliza la metaheuristica que brindará el vector de pesos
+def get_weights(n_var, pop_size, xl, xu, n_gen=10):
+    problem = ProblemaEsfera(n_var=n_var, xl=xl, xu=xu)
+    algorithm = PSO(pop_size=pop_size)
+
+    solution = minimize(
+        problem = problem,
+        algorithm = algorithm,
+        termination = ('n_gen', n_gen),
+    )
+
+    return (solution.X)
