a3f08b1bec9c3ea0d4ebd2b5df2ca8d4e6af2078
IlseAdriana <ilseadrianapa@gmail.com>
2023-07-18 22:08:06 -0600

    Datasets were added

---
+++ load_datasets.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def load_wdbc():
+    df = pd.read_csv('datasets/breast-cancer-wisconsin/wdbc.data', sep=',', header=None)
+    X = np.asarray(df.iloc[:, 2:], dtype=float)
+    y = np.where(df.iloc[:, 1] == 'B', 0, 1)
+    
+    return X, y
+
+def load_wobc():
+    df = pd.read_csv('datasets/breast-cancer-wisconsin/wobc.data', sep=',', header=None)
+    X = np.asarray(df.iloc[:, 1:-1], dtype=int)
+    y = np.where(df.iloc[:, -1] == 2, 0, 1)
+
+    return X, y
+
+def load_wpbc():
+    df = pd.read_csv('datasets/breast-cancer-wisconsin/wpbc.data', sep=',', header=None)
+    X = np.asarray(df.iloc[:, 3:], dtype=float)
+    y = np.where(df.iloc[:, 1] == 'N', 0, 1)
+
+    return X, y
+
+
+if __name__ == '__main__':
+    X, y = load_wdbc()
+    print(X)
+    print(y)
